cc39a2a8897662bb7d44af4b4d046575b842a82a
AliciaEA <aliestrada@uamv.edu.ni>
2026-01-30 17:05:18 -0600

    i did a sidebar for mobileee

--- index.js
+++ index.js
@@ -78,6 +78,11 @@ function clearFavorites() {
     renderFavorites();
 }
 
+function toggleMobileMenu(){
+    const menu = document.getElementById('mobile-menu')
+    menu.classList.toggle('hidden')
+}
+
 fetchCat();
 renderFavorites();
 
